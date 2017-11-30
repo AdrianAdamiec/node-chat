de8ab2628198e893b9ebda32df1fa2526cbbda64
adrianadamiec <adrianadamiec@gmail.com>
2017-11-30 21:44:27 +0100

    add form messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,29 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (email) {
-    console.log('New message: ', JSON.stringify(email, undefined, 2));
+socket.on('newMessage', function (message) {
+    console.log('New message: ', JSON.stringify(message, undefined, 2));
+
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Adi',
+//     text: 'test'
+// }, function (data) {
+//     console.log('OK', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name="message"]').val()
+    }, function (data) {
+        console.log(data);
+    });
 });
