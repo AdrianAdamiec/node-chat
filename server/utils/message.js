1c7a86da4373ae9240083360150b637cc12f04cb
adrianadamiec <adrianadamiec@gmail.com>
2017-12-01 07:47:19 +0100

    add timestamp messages

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,14 @@
+const moment = require('moment');
+
 let generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 };
 let generateLocationMessage = (from, latitude, longitude) => {
     let link = `https://www.google.com/maps?q=${latitude},${longitude}`;
-    return {from, link, createdAt: new Date().getTime()}
+    return {from, link, createdAt: moment().valueOf()}
 };
 module.exports = {generateMessage, generateLocationMessage};
